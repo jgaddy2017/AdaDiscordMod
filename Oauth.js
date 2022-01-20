7b0c66529efb50def471dfd542c62842582fab6b
Chris Bracey <christopherjbracey@gmail.com>
2022-01-20 16:02:27 -0500

    Cleaned up some stuff.  Also added client_ID to the .env file

--- Oauth.js
+++ Oauth.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
 const open = require('open');
 const qs = require('qs');
+require('dotenv');
 
 const getAdasDailyMods = require('./APICalls/GetAdasDailyMods');
 
-async function OauthFunc(){
-    //await open('https://www.bungie.net/en/OAuth/Authorize?client_id=39046&response_type=code');
-    let code = '7436485ce624ad7a3a620acc12767ee9';
+async function OauthFunc(MembershipID, CharacterID, AdaID, Code){
+    const ClientID = process.env.CLIENT_ID;
     let data = qs.stringify({
-        'client_id': '39046',
+        'client_id': ClientID,
         'grant_type': 'authorization_code',
-        'code': code 
+        'code': Code 
       });
       let config = {
         method: 'post',
@@ -22,10 +22,7 @@ async function OauthFunc(){
       };
       await axios(config).then(response => {
         authorizationCode = 'Bearer ' + response.data.access_token;
-        let destinyMembershipID = '4611686018487538722';
-        let myCharacterId = '2305843009446664091';
-        let adaId = '350061650'
-        getAdasDailyMods(destinyMembershipID, myCharacterId, adaId, authorizationCode);
+        getAdasDailyMods(MembershipID, CharacterID, AdaID, authorizationCode);
         })
         .catch(function (error) {
           console.log(error);
--- app.js
+++ app.js
@@ -15,6 +15,7 @@ let playerID = '21059811'
 let destinyMembershipID = '4611686018487538722';
 let myCharacterId = '2305843009446664091';
 let adaId = '350061650'
+let code = 'c8c0a6af790e0fe38ef9ebf82c7c8e8b';
 //getInventoryItemName(itemID);
 //playerInformation(playerID);
 //vendorInformation();
@@ -25,7 +26,7 @@ let adaId = '350061650'
 //EntityDefinition('this doesnt matter');
 
 //searchDestinyPlayer('Darkguard190', '3556');
-Oauth()
+Oauth(destinyMembershipID, myCharacterId, adaId, code)
 //searchDestinyPlayer('Dakiroenus');
 
 ///////ADA-1 number: 350061650
